20baf73f7aa7f03dd53df595a933abb92eb4b7ea
Durvesh N. Patil <86490574+durveshnp@users.noreply.github.com>
2022-03-23 09:48:59 +0530

    Time: 4 ms (27.86%), Space: 5.9 MB (73.18%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        if(startValue == target)
+            return 0;
+        if(target < startValue)
+            return (startValue-target);
+        
+        if(target%2 == 0)
+        {
+            return 1+brokenCalc(startValue,target/2);
+        }
+        else
+        {
+            return 1+brokenCalc(startValue,target+1);
+        }
+    }
+};
