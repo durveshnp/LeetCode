9b3a412cd7b754bb56d67c8ba034a2a03271fdc7
Durvesh N. Patil <86490574+durveshnp@users.noreply.github.com>
2022-03-31 23:30:35 +0530

    Time: 1388 ms (5.02%), Space: 8.3 MB (7.04%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        int n = nums.size();
+        
+        vector<unsigned int> pre(n, 1);
+        vector<vector<unsigned int>> dp(n + 1, vector<unsigned int>(m + 1, INT_MAX));
+        pre[0] = nums[0];
+        
+        for(int i = 1; i < n; i++)
+        {
+            pre[i] = pre[i - 1] + nums[i];
+        }
+        
+        
+        for(int i = 0; i < n; i++)
+        {
+            dp[i][1] = pre[i];
+        }
+        
+        dp[0][0] = 1;
+    
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 2; j <= m; j++)
+            {
+                for(int k = 0; k < i; k++)
+                {
+                    dp[i][j] = min(dp[i][j], max(dp[k][j - 1], pre[i] - pre[k]));
+                }
+            }
+        }
+        
+        return dp[n - 1][m];
+    }
+};
