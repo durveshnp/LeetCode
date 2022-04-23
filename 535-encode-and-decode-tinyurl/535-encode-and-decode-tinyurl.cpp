2f6f6d133255a0fba9e2d307ec5e41b3ac489a45
Durvesh N. Patil <86490574+durveshnp@users.noreply.github.com>
2022-04-23 22:22:26 +0530

    Time: 3 ms (83.88%), Space: 7.6 MB (5.80%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     
-    // Encodes a URL to a shortened URL.
     string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     unordered_map<string, string> url,code;
     
@@ -16,19 +15,15 @@ public:
         if (url.find(longUrl) != url.end()) 
             return url[longUrl];
         string key = getCode();
-        // while (code.find(key) != code.end()) 
-        //     key = getCode();
+        
         code[key] = longUrl;
         url[longUrl] = key;
         return key;
     }
 
-    // Decodes a shortened URL to its original URL.
+
     string decode(string shortUrl) {
         return code[shortUrl];
     }
 };
 
-// Your Solution object will be instantiated and called as such:
-// Solution solution;
-// solution.decode(solution.encode(url));
