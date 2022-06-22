822703e58011f85732082c57ca717a577ce585a5
Durvesh N. Patil <86490574+durveshnp@users.noreply.github.com>
2022-06-22 13:06:30 +0530

    Added solution - LeetHub

---
+++ Arrange Consonants and Vowels - GFG/arrange-consonants-and-vowels.cpp
@@ -0,0 +1,108 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	char data;
+	struct Node *next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+void printlist(Node *head)
+{
+	if (head==NULL)return;
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+void append(struct Node** headRef, char data)
+{
+	struct Node* new_node = new Node(data);
+	struct Node *last = *headRef;
+
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    return;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return;
+}
+
+// task is to complete this function
+struct Node* arrange(Node *head);
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        char tmp;
+        struct Node *head = NULL;
+        cin>>n;
+        while(n--){
+            cin>>tmp;
+            append(&head, tmp);
+        }
+        head = arrange(head);
+        printlist(head);
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+	char data;
+	struct Node *next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+*/
+// task is to complete this function
+// function should return head to the list after making 
+// necessary arrangements
+struct Node* arrange(Node *head)
+{
+   //Code here
+   struct Node *x=head;
+  vector<char>v;
+  while(x!=NULL){
+      char c=x->data;
+      if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')v.push_back(c);
+      x=x->next;
+  }
+  x=head;
+  while(x!=NULL){
+      char c=x->data;
+      if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u')v.push_back(c);
+      x=x->next;
+  }
+  x=head;
+  for(char c:v){
+      x->data=c;
+      x=x->next;
+  }
+  return head;
+}
