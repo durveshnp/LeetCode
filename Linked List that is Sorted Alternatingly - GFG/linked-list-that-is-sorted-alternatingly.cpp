31e0f21473f8135dc2cf6a1e8580c0e499828c2d
Durvesh N. Patil <86490574+durveshnp@users.noreply.github.com>
2022-06-22 17:26:10 +0530

    Added solution - LeetHub

---
+++ Linked List that is Sorted Alternatingly - GFG/linked-list-that-is-sorted-alternatingly.cpp
@@ -0,0 +1,106 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+
+void sort(Node **head);
+
+
+void append(struct Node** headRef, struct Node** tailRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    *tailRef = *headRef;
+	    return;
+	}
+	
+	(*tailRef) -> next = new_node;
+	*tailRef = (*tailRef) -> next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    struct Node* head = NULL;
+	    struct Node* tail = NULL;
+	    int n, k;
+	    cin>>n;
+	    for(int i=0; i<n ;i++){
+	        cin>>k;
+	        append(&head, &tail, k);
+	    }
+	    sort(&head);
+	    printList(head);
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*Structure of Node of the linked list is as
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+*/
+// your task is to complete this function
+
+
+
+void sort(Node **head)
+{
+     // Code here
+     
+     struct Node *temp=*head;
+    map<int,int>m;
+    while(temp) 
+    {
+        m[temp->data]++;
+        temp=temp->next;
+    }
+    temp=*head;
+    for(auto it:m)
+    {
+        while(it.second)
+        {
+            temp->data=it.first;
+            temp=temp->next;
+            it.second--;
+        }
+    }
+     
+     
+}
