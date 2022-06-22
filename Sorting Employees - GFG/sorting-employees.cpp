14665fe157c01d2f5811ec957c954f6d82bb19fc
Durvesh N. Patil <86490574+durveshnp@users.noreply.github.com>
2022-06-22 19:00:36 +0530

    Added solution - LeetHub

---
+++ Sorting Employees - GFG/sorting-employees.cpp
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+	int salary;
+	string name;
+};
+
+ // } Driver Code Ends
+/* struct node{          // node structure
+     int salary;
+     string name;
+ };
+*/
+
+
+
+class Solution{
+
+	public:
+	
+	static bool comp(node a,node b)
+	{
+	    if(a.salary == b.salary)
+	        return a.name < b.name;
+	   
+	   return a.salary < b.salary;
+	}
+	
+	void sortRecords(node arr[], int n)
+	{
+	    // Your code goes here
+	      sort(arr,arr+n,comp);
+	}
+	 
+
+};
+
+// { Driver Code Starts.
+
+
+
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+        cin>>n;
+        node arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i].name>>arr[i].salary;
+        
+
+        Solution ob;
+        ob.sortRecords(arr, n);
+        for(int i=0;i<n;i++)
+        	cout<<arr[i].name<<" "<<arr[i].salary<<" ";
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+  // } Driver Code Ends
